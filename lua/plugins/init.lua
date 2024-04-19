401b269939a6504c15d6edf2fefc22b525ffc29a
Olaf Bergner <olaf.bergner@zalando.de>
2024-04-19 21:28:39 +0200

    Move rustaceanvim config from ftype plugin to on attach. It works better this way.@

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,6 +67,9 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^4", -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function()
+      require "configs.rustaceanvim"
+    end,
   },
 
   {
